10d1be7c1b5eaa9ebeaca7d6826de109d9138f97
Paul Muscat <122230134+pmuscat41@users.noreply.github.com>
2025-08-16 17:08:43 +0000

    commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ensure we're in the right directory
+cd /workspaces/Rouse-MENA_INFO
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+echo "🎉 Jupyter mapping environment is ready!"
+echo "📓 Open map.ipynb and start coding!"
