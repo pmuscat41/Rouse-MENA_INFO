b575813d20e133229891ea822b0ebd9c6c9d61cf
Paul Muscat <122230134+pmuscat41@users.noreply.github.com>
2025-08-16 21:58:20 +0000

    working milestone

---
+++ viewhtml.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+file_path = "/workspaces/Rouse-MENA_INFO/Rouse_MEA_Patents_Designs_OnePagers_v1.html"
+print(f"Opening file: {file_path}")
+webbrowser.get("firefox").open(file_path)
